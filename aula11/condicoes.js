e407bdf61a3417612a172b7d643822deeb673794
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-08-13 00:44:03 -0300

    atualizei

---
+++ aula11/condicoes.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log (`A velocidade do seu carro é ${vel}km/h`)
+if( vel >60){ //Condição simples
+    console.log(`Você ultrapassou a velocidade máxima permitida. MULTADO`)
+}
+console.log('Dirige sempre utilizando sinto de segurança!')
