92d10d3c2328acee32be682dfb306e0b1e8930e6
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-08-22 19:44:11 -0300

    atualizei

---
+++ JAVASCRIPT2/aula3/script.js
@@ -0,0 +1,12 @@
+let pessoa = "Dimitri";
+//O JS ignora espaços em branco diferentemente de outras linguagens de programação
+//O JS aceita quebra de linhas
+document.getElementById('texto'). 
+innerHTML = `Esse texto 
+foi escrito em JS`;
+//são boas maneiras manter sempre o sua linha de cód numa mesma linha, até porque, outras linguagens de programação não aceitam quebras de linha
+
+
+function minhaFuncao(){
+
+} // é uma função, e tudo que estiver dentro de suas {}, será pertinente a função
