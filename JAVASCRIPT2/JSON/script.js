ae26dd21ad2ddb5150248fa3f07b8587d63280e0
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-08-25 21:50:05 -0300

    atualizei

--- JAVASCRIPT2/JSON/script.js
+++ JAVASCRIPT2/JSON/script.js
@@ -8,5 +8,35 @@ Ele é usado principalmente para transmitir dados entre sistemas de forma simple
 Para trabalhar com JSON em JavaScript usamos dois métodos:
 
 JSON.parse() -> Converte texto do padrão JSON em objetos
-JSON.stringify() -> Converte objetos em texto padrão JSON
+JSON.stringify() -> Converte objetos em texto padrão JSON   
+
 */
+/*const carro = {
+    marca:'fiat',
+    modelo: 'uno',
+    motor:['1.6', '1.4', '1.0']
+}
+//CONVERTEU O TEXO NO NOSSO HTML
+let texto = JSON.stringify(carro);
+//COLOCOU O TEXTO EM NOSSO HTML
+document.getElementById('area').innerHTML = texto
+//CONVERTERU O TEXTO JSON EM OBJETO
+let obj = JSON.parse(texto)
+//MOSTOU UM OBJETO MODELO EM NOSSO CONSOLE
+console.log(obj.motor[1]) */
+
+
+ function buscarCep(){
+    let campoTexto = document.getElementById('cep').value
+
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', `https://viacep.com.br/ws/${campoTexto }/json/`)
+    ajax.send();
+    ajax.onload = function(){
+        let obj = JSON.parse(this.responseText)
+        let logradouro = obj.logradouro
+        let city = obj.localidade
+        let cep = obj.cep
+        alert(`${ logradouro} ${cep} ${city}`)
+    }
+ }
