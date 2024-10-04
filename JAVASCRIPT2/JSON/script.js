e39a513f7368ddf7cd0eeb24d3a43b926078a44e
Breno Weslley Santana Mattos <bwsantana68@gmail.com>
2024-10-03 22:24:20 -0300

    Update script.js

--- JAVASCRIPT2/JSON/script.js
+++ JAVASCRIPT2/JSON/script.js
@@ -3,7 +3,7 @@ JSON
 JSON significa JavaScript Object Notation que traduzido para o português fica algo como, notação de objeto em JavaScript.
 Explicando de modo simples, JSON é basicamente uma forma de converter um objeto em texto e o contrário também, um texto em um objeto. 
 
-Ele é usado principalmente para transmitir dados entre sistemas de forma simples, já que o formato de  texto é lido praticamente toda linguagem de programação.
+Ele é usado principalmente para transmitir dados entre sistemas de forma simples, já que o formato de  texto é lido praticamente em toda linguagem de programação.
 
 Para trabalhar com JSON em JavaScript usamos dois métodos:
 
